8888207948f51413ca603fece7373aa1cf5d458c
dawe484 <dawe484@gmail.com>
2017-09-20 21:18:57 +0200

    Let and Const Declaration
    In file src/index.js is code showing how to start using the let and
    const declaration for setting variables and constants in ES6.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-let test = 'Hello';
+'use strict';
 
-class User {
-  
-  register() {
-    console.log('User Registered...');
+//var a = 'test a';
+//let b = 'test b';
+
+//console.log(a);
+//console.log(b);
+
+function testVar() {
+  var a = 30;
+  if (true) {
+    var a = 50;
+    console.log('var', a);
   }
-  
+  console.log('var', a);
 }
+
+function testLet() {
+  let a = 30;
+  if (true) {
+    let a = 50;
+    console.log('let', a);
+  }
+  console.log('let', a);
+}
+
+testVar();
+testLet();
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//console.log(i); // Uncaught ReferenceError: i is not defined
+
+const colors = [];
+
+colors.push('red');
+colors.push('blue');
+
+//colors = 'green'; // Uncaught TypeError: Assignment to constant variable.
+
+console.log(colors);
